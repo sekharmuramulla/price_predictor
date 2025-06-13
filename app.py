a1e12e55e7165553a85a8474eaacc8a7db023b2b
sekharmuramulla <msomasekhar44@gmail.com>
2025-06-13 22:31:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,4 +43,4 @@ def predict():
         return render_template('index.html', prediction_text=f"Error: {e}")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
